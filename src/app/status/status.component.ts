46d51bd1fae700714577daa14b682105b6379629
Nico Jensch <root@dr460nf1r3.org>
2024-06-03 12:48:57 +0200

    feat(status): add package information, optimize style

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Axios } from "axios";
+import { CurrentPackages, CurrentQueue, PackagesObject, StatsObject } from "../types";
 
 @Component({
     selector: "app-status",
@@ -9,7 +10,8 @@ import { Axios } from "axios";
     styleUrl: "./status.component.css",
 })
 export class StatusComponent {
-    currentQueue: any[] = [];
+    currentPackages: CurrentPackages = [];
+    currentQueue: CurrentQueue = [];
     currentMetrics = {
         database_queue: {
             completed: 0,
@@ -37,22 +39,48 @@ export class StatusComponent {
         this.axios
             .get("queue/stats")
             .then((response) => {
-                const waiting = { waiting: { count: 1, packages: ["test1", "test2]", "test3"] } };
-                const currentQueue = [];
-                currentQueue.push(waiting);
-                console.log("currentQueue", currentQueue);
-                console.log("length", currentQueue.length);
+                const currentQueue: StatsObject = JSON.parse(response.data);
                 if (currentQueue.length > 0) {
                     for (const index in currentQueue) {
-                        console.log("queue", index);
+                        const nameWithoutRepo: string[] = [];
+                        Object.values(currentQueue[index])[0].packages.forEach((pkg): void => {
+                            if (pkg !== undefined) {
+                                nameWithoutRepo.push(pkg.split("/")[1]);
+                            }
+                        });
                         this.currentQueue.push({
                             status: Object.keys(currentQueue[index])[0],
                             count: Object.values(currentQueue[index])[0].count,
-                            packages: Object.values(currentQueue[index])[0].packages,
+                            packages: nameWithoutRepo,
                         });
                     }
                 }
-                console.log(this.currentQueue);
+            })
+            .catch((error: any) => {
+                console.error(error);
+            });
+    }
+
+    async getPackageStats(): Promise<void> {
+        this.axios
+            .get("queue/packages")
+            .then((response) => {
+                const currentPackages: PackagesObject = JSON.parse(response.data);
+                if (currentPackages.length > 0) {
+                    for (const index in currentPackages) {
+                        const pkgTime = new Date(Object.values(currentPackages[index])[0].timestamp).toLocaleString(
+                            "en-GB",
+                            { timeZone: "UTC" },
+                        );
+                        this.currentPackages.push({
+                            name: Object.keys(currentPackages[index])[0],
+                            arch: Object.values(currentPackages[index])[0].arch,
+                            srcrepo: Object.values(currentPackages[index])[0].srcrepo,
+                            time: pkgTime,
+                            repo_files: Object.values(currentPackages[index])[0].repo_files,
+                        });
+                    }
+                }
             })
             .catch((error: any) => {
                 console.error(error);
@@ -62,6 +90,7 @@ export class StatusComponent {
     async updateAll(): Promise<void> {
         await this.getMetrics();
         await this.getQueueStats();
+        await this.getPackageStats();
         this.lastUpdated = new Date().toLocaleString("en-GB", { timeZone: "UTC" });
     }
 
