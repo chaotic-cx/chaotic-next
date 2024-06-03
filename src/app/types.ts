069767a72a3744e6586a457c8c5d75c19987e71b
Nico Jensch <root@dr460nf1r3.org>
2024-06-03 12:48:57 +0200

    feat(status): add package information, optimize style

--- src/app/types.ts
+++ src/app/types.ts
@@ -8,3 +8,37 @@ export interface metricsObject {
         failed: number;
     };
 }
+
+export type StatsObject = Record<
+    string,
+    {
+        count: number;
+        packages: (string | undefined)[];
+    }
+>[];
+
+export type PackagesObject = Record<
+    string,
+    {
+        arch: string;
+        srcrepo: string;
+        timestamp: string;
+        repo_files: string;
+    }
+>[];
+
+export interface QueueStatus {
+    status: string;
+    count: number;
+    packages: (string | undefined)[];
+}
+export type CurrentQueue = QueueStatus[];
+
+export interface CurrentPackage {
+    name: string;
+    arch: string;
+    srcrepo: string;
+    time: string;
+    repo_files: string;
+}
+export type CurrentPackages = CurrentPackage[];
