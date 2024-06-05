cc283fd83dc3fa2f03a26e6e8653e04e7c304e2c
Nico Jensch <root@dr460nf1r3.org>
2024-06-05 18:42:16 +0200

    feat: add docs / about text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { NgOptimizedImage } from "@angular/common";
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { ChaoticAttractorComponent } from "../chaotic-attractor/chaotic-attractor.component";
+import { MirrorMapComponent } from "../mirror-map/mirror-map.component";
 
 @Component({
     selector: "app-home",
     standalone: true,
-    imports: [NgOptimizedImage, RouterLink, RouterLinkActive, ChaoticAttractorComponent],
+    imports: [NgOptimizedImage, RouterLink, RouterLinkActive, ChaoticAttractorComponent, MirrorMapComponent],
     templateUrl: "./home.component.html",
     styleUrl: "./home.component.css",
 })
