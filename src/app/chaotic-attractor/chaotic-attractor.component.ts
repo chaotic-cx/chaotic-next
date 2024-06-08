9938bf27106a4513c15a4a21831f2691c86a5d48
Nico Jensch <root@dr460nf1r3.org>
2024-06-08 20:59:16 +0200

    fix(design): no animation on mobile; now everything should be padded equally
    Performance of the Aizawa applet is sadly subpar on mobile, therefore simply display our logo here.

--- src/app/chaotic-attractor/chaotic-attractor.component.ts
+++ src/app/chaotic-attractor/chaotic-attractor.component.ts
@@ -1,4 +1,4 @@
-import { NgOptimizedImage, isPlatformBrowser } from "@angular/common"
+import { isPlatformBrowser, NgOptimizedImage } from "@angular/common"
 import { Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from "@angular/core"
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser"
 import iframeResizer from "@iframe-resizer/parent"
@@ -18,6 +18,8 @@ export class ChaoticAttractorComponent {
     url = CAUR_HOME_URL + "aizawa"
     urlSafe: SafeResourceUrl
     displayInteractive: boolean = true
+    isMobile: boolean = false
+
     // @ts-ignore
     @ViewChild("iframe", { static: false }) iframe: ElementRef
 
@@ -27,8 +29,20 @@ export class ChaoticAttractorComponent {
     ) {
         this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url)
         this.checkIfExists(platformId)
+        this.isMobile = this.checkIfMobile()
     }
 
+    ngAfterViewInit(): void {
+        this.iframe.nativeElement.onload = () => {
+            iframeResizer({ license: "GPLv3", direction: "horizontal" }, this.iframe.nativeElement)
+        }
+    }
+
+    /**
+     * Check if the platform is a browser and then checks if the iframe URL exists.
+     * @param platformId The platform ID.
+     * @private
+     */
     private checkIfExists(platformId: Object): void {
         if (isPlatformBrowser(platformId)) {
             const request = new XMLHttpRequest()
@@ -40,9 +54,13 @@ export class ChaoticAttractorComponent {
         }
     }
 
-    ngAfterViewInit(): void {
-        this.iframe.nativeElement.onload = () => {
-            iframeResizer({ license: "GPLv3", direction: "horizontal" }, this.iframe.nativeElement)
-        }
+    /**
+     * Check if the user is on a mobile device.
+     * https://dev.to/timhuang/a-simple-way-to-detect-if-browser-is-on-a-mobile-device-with-javascript-44j3
+     * @returns True if the user is on a mobile device, false otherwise.
+     * @private
+     */
+    private checkIfMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     }
 }
