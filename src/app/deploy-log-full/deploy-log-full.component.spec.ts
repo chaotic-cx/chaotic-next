e8baacda53447662d1929298431c2c6acf031d40
Nico Jensch <root@dr460nf1r3.org>
2024-06-09 14:19:53 +0200

    feat(deploy-log): new page for full deployments

---
+++ src/app/deploy-log-full/deploy-log-full.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+
+import { DeployLogFullComponent } from "./deploy-log-full.component"
+
+describe("DeployLogFullComponent", () => {
+    let component: DeployLogFullComponent
+    let fixture: ComponentFixture<DeployLogFullComponent>
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [DeployLogFullComponent],
+        }).compileComponents()
+
+        fixture = TestBed.createComponent(DeployLogFullComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it("should create", () => {
+        expect(component).toBeTruthy()
+    })
+})
