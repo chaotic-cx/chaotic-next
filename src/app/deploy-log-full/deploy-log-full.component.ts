8626804f402477fdd64eb96f067332626ba6c0e1
Nico Jensch <root@dr460nf1r3.org>
2024-06-09 14:59:11 +0200

    feat: allow searching n amount of logs; usability enhancements

--- src/app/deploy-log-full/deploy-log-full.component.ts
+++ src/app/deploy-log-full/deploy-log-full.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core"
+import { FormsModule } from "@angular/forms"
 import TimeAgo from "javascript-time-ago"
 import en from "javascript-time-ago/locale/en"
 import { DeploymentList } from "../types"
@@ -7,18 +8,28 @@ import { getDeployments, parseTgMessage } from "../utils/utils"
 @Component({
     selector: "app-deploy-log-full",
     standalone: true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl: "./deploy-log-full.component.html",
     styleUrl: "./deploy-log-full.component.css",
 })
 export class DeployLogFullComponent {
     latestDeployments: DeploymentList = []
+    logAmount: number | undefined
 
     constructor() {
         TimeAgo.addDefaultLocale(en)
     }
 
     async ngAfterViewInit(): Promise<void> {
-        this.latestDeployments = parseTgMessage(await getDeployments(100))
+        void this.updateLogAmount(100)
+    }
+
+    async updateLogAmount(amount: number) {
+        this.latestDeployments = parseTgMessage(await getDeployments(amount + 1))
+    }
+
+    async getNewAmount(): Promise<void> {
+        // @ts-ignore
+        void this.updateLogAmount(parseInt(this.logAmount))
     }
 }
