6fcf1a331273abadd6e720ef9c603bddcabd84b5
Nico Jensch <root@dr460nf1r3.org>
2024-06-13 19:19:34 +0200

    fix(mirrorlist): add back file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,38 +11,42 @@ import { StatusComponent } from "./status/status.component"
 
 export const routes: Routes = [
     {
+        title: "Chaotic-AUR",
         path: "",
         component: HomeComponent,
     },
     {
+        title: "Build Status",
         path: "status",
         component: StatusComponent,
     },
     {
+        title: "Deploy Log",
         path: "deploy-log",
         component: DeployLogFullComponent,
     },
     {
+        title: "Memorial",
         path: "memorial",
         component: MemorialComponent,
     },
     {
+        title: "About",
         path: "about",
         component: AboutComponent,
     },
     {
+        title: "Documentation",
         path: "docs",
         component: DocsComponent,
     },
     {
-        path: "memorial",
-        component: MemorialComponent,
-    },
-    {
+        title: "Package Stats",
         path: "stats",
         component: StatsPage,
     },
     {
+        title: "Not Found",
         path: "not-found",
         component: NotFoundComponent,
     },
