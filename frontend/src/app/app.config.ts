3812c71f8f8a128b1b7401b016ac9c802dc9bdaf
Nico Jensch <root@dr460nf1r3.org>
2024-06-16 20:07:40 +0200

    fix(status): treat higher package counts better

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,8 @@
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
 import { ApplicationConfig } from "@angular/core"
 import { provideRouter } from "@angular/router"
 import { provideHighlightOptions } from "ngx-highlightjs"
 import { routes } from "./app.routes"
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
 
 export const appConfig: ApplicationConfig = {
     providers: [
