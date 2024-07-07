1c52bf34e762ecff0ef2f0691ce93c2cc9ab4252
Nico Jensch <root@dr460nf1r3.org>
2024-07-07 12:37:13 +0200

    fix: add disclaimer about non-live data; update pkglist to live

--- shared-lib/src/lib/types.ts
+++ shared-lib/src/lib/types.ts
@@ -12,7 +12,7 @@ export const CAUR_HOME_URL = "https://aur.chaotic.cx/"
 export const CAUR_MAP_URL = "https://status.chaotic.cx/map"
 export const CAUR_METRICS_URL = "https://metrics.chaotic.cx/"
 export const CAUR_NEWS_ID = "-1001293714071"
-export const CAUR_PKG_LIST_URL = "https://builds.garudalinux.org/chaotic-v4/pkgs.files.txt"
+export const CAUR_PKG_LIST_URL = "https://builds.garudalinux.org/repos/chaotic-aur/pkgs.files.txt"
 export const CAUR_PRIMARY_KEY = "3056513887B78AEB"
 export const CAUR_TG_API_URL = `${CAUR_BACKEND_URL}/telegram/`
 
