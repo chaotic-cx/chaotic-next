ccf932cffe06fbfd18d3dea6796941a03d2a7161
Nico Jensch <root@dr460nf1r3.org>
2024-10-05 11:43:37 +0200

    fix: failure notifications; httpLogger middleware

---
+++ backend/src/http-logger/httplogger.middleware.spec.ts
@@ -0,0 +1,7 @@
+import { HttpLoggerMiddleware } from "./httplogger.middleware"
+
+describe('HttpLoggerMiddleware', () => {
+  it('should be defined', () => {
+    expect(new HttpLoggerMiddleware()).toBeDefined();
+  });
+});
--- backend/src/misc/misc.service.ts
+++ backend/src/misc/misc.service.ts
@@ -1,11 +1,16 @@
 import { CAUR_PKG_LIST_URL, type PkgListRetObject } from "@./shared-lib"
-import { Injectable } from "@nestjs/common"
+import { Injectable, Logger } from "@nestjs/common"
 import { Axios } from "axios"
 
 @Injectable()
 export class MiscService {
+    constructor() {
+        Logger.log("MiscService initialized", "MiscService")
+    }
+
     /**
      * Return the list of packages from Chaotic-AUR, mainly just for CORS proxying and caching.
+     * @returns The list of packages from the specified package list.
      */
     async getPkgList(): Promise<PkgListRetObject> {
         const axios = new Axios({
