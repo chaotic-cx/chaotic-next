ca27e598b5cefa018417af0696865af9237d8ff7
Nico Jensch <root@dr460nf1r3.org>
2024-10-19 12:20:11 +0200

    chore: migrate eslint configs to v9

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,45 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+import defaultExport from "../eslint.config.mjs";
+
+export default [{
+    ignores: ["!**/*"],
+}, ...defaultExport, ...compat.extends(
+    "plugin:@nx/angular",
+    "plugin:@angular-eslint/template/process-inline-templates",
+).map(config => ({
+    ...config,
+    files: ["**/*.ts"],
+})), {
+    files: ["**/*.ts"],
+
+    rules: {
+        "@angular-eslint/directive-selector": ["error", {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+        }],
+
+        "@angular-eslint/component-selector": ["error", {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+        }],
+    },
+}, ...compat.extends("plugin:@nx/angular-template").map(config => ({
+    ...config,
+    files: ["**/*.html"],
+})), {
+    files: ["**/*.html"],
+    rules: {},
+}];
---
+++ shared-lib/eslint.config.mjs
@@ -0,0 +1,44 @@
+import parser from "jsonc-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import defaultExport from "../eslint.config.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    {
+        ignores: ["!**/*"],
+    },
+    ...defaultExport,
+    {
+        files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+        rules: {},
+    },
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        rules: {},
+    },
+    {
+        files: ["**/*.js", "**/*.jsx"],
+        rules: {},
+    },
+    {
+        files: ["**/*.json"],
+
+        languageOptions: {
+            parser: parser,
+        },
+
+        rules: {
+            "@nx/dependency-checks": "error",
+        },
+    },
+];
