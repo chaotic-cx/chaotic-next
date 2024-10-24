886f8908688f7edfe9801001b3b06fd09f4c78d3
Nico Jensch <root@dr460nf1r3.org>
2024-10-24 14:00:31 +0200

    feat(backend): more typing; drop unused code

--- backend/src/builder/builder.controller.ts
+++ backend/src/builder/builder.controller.ts
@@ -1,26 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 import { BuilderService } from "./builder.service";
-import { Builder } from "./builder.entity";
 
 @Controller("builder")
 export class BuilderController {
     constructor(private builderService: BuilderService) {}
-
-    @Get()
-    getBuilder() {
-        return "Builder";
-    }
-
-    @Get("test")
-    test() {
-        const builder = {
-            builderClass: "1",
-            name: "test name",
-            description: "test desc",
-            builds: [],
-            isActive: false,
-        };
-        // @ts-ignore
-        return this.builderService.addBuilder(builder);
-    }
 }
