3c9b7a6f0d374db45422912c960bdcd2e5e585e4
Nico Jensch <root@dr460nf1r3.org>
2024-10-24 14:40:08 +0200

    fix(backend): more robust error handling

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -7,3 +7,9 @@ export const PG_OPTIONS = {
     entities: [`${__dirname}/*/*.entity.{js,ts}`],
     synchronize: process.env.NODE_ENV === "development",
 };
+
+export const REDIS_OPTIONS = {
+     host: process.env.REDIS_HOST || "localhost",
+     port : Number(process.env.REDIS_PORT) || 6379,
+     password:  process.env.REDIS_PASSWORD || ""
+}
