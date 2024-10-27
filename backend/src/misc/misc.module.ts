bd0701ed9f93df9a22a7e3db82206ee1bd883ce9
Nico Jensch <root@dr460nf1r3.org>
2024-10-27 17:21:50 +0100

    fix(backend): re-add routes

--- backend/src/misc/misc.module.ts
+++ backend/src/misc/misc.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { MiscController } from "./misc.controller";
 import { MiscService } from "./misc.service";
+import { CacheModule } from "@nestjs/cache-manager";
 
 @Module({
+    imports: [CacheModule.register()],
     controllers: [MiscController],
     providers: [MiscService],
 })
