2e3e039fe8b2cb02da10534a62c86ada648d0f38
Nico Jensch <root@dr460nf1r3.org>
2024-10-27 16:06:14 +0100

    fix(backend): public routes be public

--- backend/src/router/router.module.ts
+++ backend/src/router/router.module.ts
@@ -6,13 +6,11 @@ import { RouterService } from "./router.service";
 import { RouterController } from "./router.controller";
 import { APP_GUARD } from "@nestjs/core";
 import { JwtGuard } from "../auth/jwt.auth.guard";
-import { ConfigModule } from "@nestjs/config";
-import routerConfig from "../config/router.config";
 
 @Module({
     controllers: [RouterController],
     exports: [TypeOrmModule],
-    imports: [ConfigModule.forFeature(routerConfig), TypeOrmModule.forFeature([RouterHit, Package, Repo, Mirror])],
+    imports: [TypeOrmModule.forFeature([RouterHit, Package, Repo, Mirror])],
     providers: [RouterService, { provide: APP_GUARD, useClass: JwtGuard }],
 })
 export class RouterModule {}
--- backend/src/telegram/telegram.controller.ts
+++ backend/src/telegram/telegram.controller.ts
@@ -2,17 +2,20 @@ import { CacheInterceptor } from "@nestjs/cache-manager";
 import { Controller, Get, Param, ParseIntPipe, Query, UseInterceptors } from "@nestjs/common";
 import { TelegramService } from "./telegram.service";
 import { RepositoryList, TgMessageList } from "@./shared-lib";
+import { AllowAnonymous } from "../auth/anonymous.decorator";
 
 @Controller("telegram")
 @UseInterceptors(CacheInterceptor)
 export class TelegramController {
     constructor(private telegramService: TelegramService) {}
 
+    @AllowAnonymous()
     @Get("news")
     getNews(): Promise<TgMessageList> {
         return this.telegramService.getNews();
     }
 
+    @AllowAnonymous()
     @Get("deployments/all/:amount")
     getTelegram(
         @Param("amount", ParseIntPipe) amount: number,
@@ -21,6 +24,7 @@ export class TelegramController {
         return this.telegramService.getDeployments(amount, repo);
     }
 
+    @AllowAnonymous()
     @Get("deployments/succeeded/:amount")
     getSucceeded(
         @Param("amount", ParseIntPipe) amount: number,
@@ -29,6 +33,7 @@ export class TelegramController {
         return this.telegramService.getSucceeded(amount, repo);
     }
 
+    @AllowAnonymous()
     @Get("deployments/failed/:amount")
     getFailed(
         @Param("amount", ParseIntPipe) amount: number,
@@ -37,6 +42,7 @@ export class TelegramController {
         return this.telegramService.getFailed(amount, repo);
     }
 
+    @AllowAnonymous()
     @Get("deployments/timeout/:amount")
     getTimedOut(
         @Param("amount", ParseIntPipe) amount: number,
@@ -45,6 +51,7 @@ export class TelegramController {
         return this.telegramService.getTimedOut(amount, repo);
     }
 
+    @AllowAnonymous()
     @Get("deployments/cleanup/:amount")
     getCleanupJobs(
         @Param("amount", ParseIntPipe) amount: number,
@@ -57,7 +64,6 @@ export class TelegramController {
     deAuth(): Promise<void> {
         return this.telegramService.deAuth();
     }
-
     @Get("auth")
     auth(): void {
         this.telegramService.doAuth();
