bfb59877fd7d1c7c725f12d808054cc053ab1c0d
Nico Jensch <root@dr460nf1r3.org>
2024-10-27 15:21:00 +0100

    fix(backend): remove custom token on /router/hit route

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("app", () => ({
-    port: process.env.PORT ?? 3000,
-    host: process.env.HOST ?? "localhost",
+    port: process.env.CAUR_PORT ?? 3000,
+    host: process.env.CAUR_HOST ?? "localhost",
 }));
