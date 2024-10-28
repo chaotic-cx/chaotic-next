0f46549e097f77a4ba8f72dd233edd8509476d14
Nico Jensch <root@dr460nf1r3.org>
2024-10-28 20:11:34 +0100

    feat(backend): improve router API

--- backend/src/router/router.service.ts
+++ backend/src/router/router.service.ts
@@ -1,9 +1,10 @@
 import { BadRequestException, Injectable, Logger } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import type { Repository } from "typeorm";
-import { Package, pkgnameExists, Repo, repoExists } from "../builder/builder.entity";
+import { Package, Repo, pkgnameExists, repoExists } from "../builder/builder.entity";
+import { nDaysInPast } from "../functions";
 import type { RouterHitBody } from "../types";
-import { Mirror, mirrorExists, RouterHit, type RouterHitColumns } from "./router.entity";
+import { Mirror, RouterHit, type RouterHitColumns, mirrorExists } from "./router.entity";
 
 @Injectable()
 export class RouterService {
@@ -54,43 +55,82 @@ export class RouterService {
         void this.routerRitRepo.save(toSave);
     }
 
+    /**
+     * Get the general stats for the last x days.
+     * @param days The number of days to look back
+     * @param type The column to select
+     */
     getGeneralStats(days: number, type: RouterHitColumns): Promise<RouterHit[]> {
         return this.routerRitRepo
             .createQueryBuilder("router_hit")
             .select(`router_hit.${type}`)
-            .where("router_hit.timestamp > :date", { date: new Date(Date.now() - days * 24 * 60 * 60 * 1000) })
+            .where("router_hit.timestamp > :date", { date: nDaysInPast(days) })
             .getMany();
     }
 
-    getCountryStats(days: number): Promise<RouterHit[]> {
+    /**
+     * Get the number of hits per country for the last x days.
+     * @param days The number of days to look back
+     * @returns An array of objects containing the country name and the number of hits
+     */
+    getCountryStats(days: number): Promise<{ country: string; count: number }[]> {
         return this.routerRitRepo
             .createQueryBuilder("router_hit")
-            .select("router_hit.country")
+            .select("router_hit.country AS country")
             .addSelect("COUNT(router_hit.country)", "count")
-            .where("router_hit.timestamp > :date", { date: new Date(Date.now() - days * 24 * 60 * 60 * 1000) })
+            .where("router_hit.timestamp > :date", { date: nDaysInPast(days) })
             .groupBy("router_hit.country")
+            .orderBy("count", "DESC")
             .getRawMany();
     }
 
-    getMirrorStats(days: number): Promise<RouterHit[]> {
+    /**
+     * Get the number of hits per mirror for the last x days.
+     * @param days The number of days to look back
+     * @returns An array of objects containing the mirror hostname and the number of hits
+     */
+    getMirrorStats(days: number): Promise<{ mirror: string; count: number }[]> {
         return this.routerRitRepo
             .createQueryBuilder("router_hit")
-            .select("mirror.hostname")
+            .select("mirror.hostname AS mirror")
             .addSelect("COUNT(mirror.hostname)", "count")
             .innerJoin("router_hit.hostname", "mirror")
-            .where("router_hit.timestamp > :date", { date: new Date(Date.now() - days * 24 * 60 * 60 * 1000) })
+            .where("router_hit.timestamp > :date", { date: nDaysInPast(days) })
             .groupBy("mirror.hostname")
+            .orderBy("count", "DESC")
             .getRawMany();
     }
 
-    getPackageStats(days: number): Promise<RouterHit[]> {
+    /**
+     * Get the number of hits per package for the last x days.
+     * @param days The number of days to look back
+     * @returns An array of objects containing the package name and the number of hits
+     */
+    getPackageStats(days: number): Promise<{ pkgbase: string; count: number }[]> {
         return this.routerRitRepo
             .createQueryBuilder("router_hit")
-            .select("package.pkgname")
+            .select("package.pkgname AS pkgbase")
             .addSelect("COUNT(package.pkgname)", "count")
             .innerJoin("router_hit.pkgbase", "package")
-            .where("router_hit.timestamp > :date", { date: new Date(Date.now() - days * 24 * 60 * 60 * 1000) })
+            .where("router_hit.timestamp > :date", { date: nDaysInPast(days) })
             .groupBy("package.pkgname")
+            .orderBy("count", "DESC")
+            .getRawMany();
+    }
+
+    /**
+     * Get the number of hits per day for the last x days.
+     * @param days The number of days to look back
+     * @returns An array of objects containing the day and the number of hits
+     */
+    getPerDayStats(days: number): Promise<{ day: string; count: number }[]> {
+        return this.routerRitRepo
+            .createQueryBuilder("router_hit")
+            .select("DATE(router_hit.timestamp) AS date")
+            .addSelect("COUNT(DATE(router_hit.timestamp))", "count")
+            .where("router_hit.timestamp > :date", { date: nDaysInPast(days) })
+            .groupBy("DATE(router_hit.timestamp)")
+            .orderBy("date", "ASC")
             .getRawMany();
     }
 }
