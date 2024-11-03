5d9704cc861c1bb916d03fad425864a88770e527
Nico Jensch <root@dr460nf1r3.org>
2024-11-03 15:59:41 +0100

    feat(backend): add additional builder query routes

---
+++ frontend/src/app/chart/chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.css'
+})
+export class ChartComponent {
+
+}
