78934ae8ec535b5b5714f9b6b2781abb43497a0f
Nico Jensch <root@dr460nf1r3.org>
2024-11-08 09:50:39 +0100

    feat(backend): support 1 api token per repo

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -2,7 +2,8 @@ import { registerAs } from "@nestjs/config";
 import { IS_PROD } from "../constants";
 
 export default registerAs("app", () => ({
-    port: process.env.CAUR_PORT ?? 3000,
+    dbKey: process.env.CAUR_DB_KEY,
     host: process.env.CAUR_HOST ?? "0.0.0.0",
+    port: process.env.CAUR_PORT ?? 3000,
     prod: IS_PROD,
 }));
