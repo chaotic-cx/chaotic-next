f98b0ece93d87a24156b6c5bde759826746c1323
Nico Jensch <root@dr460nf1r3.org>
2024-11-24 14:46:43 +0100

    chore(backend): set cooldown to 7 days by default

--- backend/src/config/repo-manager.config.ts
+++ backend/src/config/repo-manager.config.ts
@@ -1,10 +1,11 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("repoMan", () => ({
+    cooldownDays: process.env.REPOMANAGER_COOLDOWN_DAYS ?? 7,
     gitAuthor: process.env.GIT_AUTHOR ?? "Temeraire",
     gitEmail: process.env.GIT_EMAIL ?? "ci@chaotic.cx",
     gitUsername: process.env.GIT_USERNAME ?? "git",
-    globalBlacklist: process.env.REPOMANAGER_NEVER_REBUILD ?? "[]",
+    globalBlocklist: process.env.REPOMANAGER_NEVER_REBUILD ?? "[]",
     globalTriggers: process.env.REPOMANAGER_ALWAYS_REBUILD ?? "[]",
     regenDatabase: process.env.REPOMANAGER_REGEN_DB ?? false,
     schedulerInterval: process.env.REPOMANAGER_SCHEDULE ?? "0 * * * *",
