9b2a41f246d65a07ca3193d05c793cd3a627b0cd
Nico Jensch <root@dr460nf1r3.org>
2024-12-31 09:55:22 +0100

    feat(frontend): progress, visual enhancements, bring back catppuccin

--- frontend-v2/src/app/home/home.component.ts
+++ frontend-v2/src/app/home/home.component.ts
@@ -2,19 +2,23 @@ import { Component } from '@angular/core';
 import { NewsfeedComponent } from '../newsfeed/newsfeed.component';
 import { ButtonDirective, ButtonIcon, ButtonLabel } from 'primeng/button';
 import { Ripple } from 'primeng/ripple';
-import { ChaoticAttractorComponent } from '../chaotic-attractor/chaotic-attractor.component';
 import { MirrorMapComponent } from '../mirror-map/mirror-map.component';
+import { RouterLink } from '@angular/router';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'chaotic-home',
   imports: [
+    AnimateOnScrollModule,
     NewsfeedComponent,
     ButtonDirective,
     Ripple,
-    ChaoticAttractorComponent,
     ButtonLabel,
     ButtonIcon,
     MirrorMapComponent,
+    RouterLink,
+    NgOptimizedImage,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
