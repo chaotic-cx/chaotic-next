941815738dc14120f790b23ba7ebc3cc4a1d268e
Nico Jensch <root@dr460nf1r3.org>
2025-01-02 14:13:03 +0100

    feat(backend): don't use paginated pipelines

--- backend/src/gitlab/gitlab.controller.ts
+++ backend/src/gitlab/gitlab.controller.ts
@@ -1,9 +1,8 @@
-import { Body, Controller, Get, Headers, Param, ParseIntPipe, Post, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Headers, Post, UnauthorizedException } from '@nestjs/common';
 import { GitlabService } from './gitlab.service';
 import { AllowAnonymous } from '../auth/anonymous.decorator';
 import { InMemoryCache } from '../cache.source';
 import { MINUTE } from 'nestjs-omacache';
-import { PipelineWebhook } from './interfaces';
 import { ConfigService } from '@nestjs/config';
 import { PipelineWithExternalStatus } from '@./shared-lib';
 
@@ -16,24 +15,26 @@ export class GitlabController {
     private readonly gitlabService: GitlabService,
   ) {}
 
+  @InMemoryCache({
+    key: 'pipelines',
+    kind: 'bust',
+  })
   @AllowAnonymous()
   @Post('update')
-  updateCache(@Body() body: PipelineWebhook, @Headers('X-Gitlab-Token') token: string) {
+  updateCache(@Headers('X-Gitlab-Token') token: string) {
     if (token !== this.WEBHOOK_TOKEN) {
       throw new UnauthorizedException('Invalid token');
     }
-    this.gitlabService.updatePipelineCache(body);
   }
 
   @InMemoryCache({
-    key: 'some',
+    key: 'pipelines',
     kind: 'temporal',
-    ttl: MINUTE,
-    paramIndex: [0],
+    ttl: 15 * MINUTE,
   })
   @AllowAnonymous()
-  @Get('pipelines/:page')
-  async getPipelines(@Param('page', ParseIntPipe) page?: number): Promise<PipelineWithExternalStatus[]> {
-    return await this.gitlabService.getLastPipelines({ page });
+  @Get('pipelines')
+  async getPipelines(): Promise<PipelineWithExternalStatus[]> {
+    return await this.gitlabService.getLastPipelines();
   }
 }
