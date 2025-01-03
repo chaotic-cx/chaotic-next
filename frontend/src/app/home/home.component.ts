e71d2fa63785eb2a34a5e3fef8b4c6f3fd9471cc
Nico Jensch <root@dr460nf1r3.org>
2025-01-03 20:55:59 +0100

    feat(frontend): new logo, updated news feed, cleanup

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { NewsfeedComponent } from '../newsfeed/newsfeed.component';
-import { ButtonDirective, ButtonLabel } from 'primeng/button';
+import { ButtonDirective, ButtonIcon, ButtonLabel } from 'primeng/button';
 import { Ripple } from 'primeng/ripple';
 import { MirrorMapComponent } from '../mirror-map/mirror-map.component';
 import { RouterLink } from '@angular/router';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { NgOptimizedImage } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'chaotic-home',
@@ -16,10 +18,28 @@ import { AnimateOnScrollModule } from 'primeng/animateonscroll';
     ButtonLabel,
     MirrorMapComponent,
     RouterLink,
+    NgOptimizedImage,
+    ButtonIcon,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit {
+  isWide = signal<boolean>(true);
+  observer = inject(BreakpointObserver);
+
+  ngOnInit() {
+    this.observer.observe('(min-width: 768px)').subscribe((result) => {
+      this.isWide.set(result.matches);
+    });
+  }
+
+  /**
+   * Many thanks for adapting the original applet and letting us use it!
+   * Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+   * Copyright (c) 2018 Juan Carlos Ponce Campuzano
+   */
+  openApplet() {
+    window.location.href = 'https://aur.chaotic.cx/aizawa/index.html';
+  }
 }
