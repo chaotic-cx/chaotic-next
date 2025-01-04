196478bb7ae857cfb2c089c4ecebe32e575b3e4d
Nico Jensch <root@dr460nf1r3.org>
2025-01-04 18:15:28 +0100

    fix(frontend): made cmd copyable

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 import { provideGarudaNG } from '@garudalinux/core';
-import { provideSFConfig } from 'ngx-highlight-js';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 import { APP_CONFIG } from '../environments/app-config.token';
 import { environment } from '../environments/environment.dev';
+import { routes } from './app.routes';
 import { Catppuccin } from './theme';
 
 export const appConfig: ApplicationConfig = {
@@ -24,9 +24,14 @@ export const appConfig: ApplicationConfig = {
         ripple: true,
       },
     ),
+    provideHighlightOptions({
+      coreLibraryLoader: () => import('highlight.js/lib/core'),
+      languages: {
+        shell: () => import('highlight.js/lib/languages/shell.js'),
+      },
+    }),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
-    provideSFConfig({ lang: 'shell' }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     { provide: APP_CONFIG, useValue: environment },
     { provide: LOCALE_ID, useValue: 'en-GB' },
