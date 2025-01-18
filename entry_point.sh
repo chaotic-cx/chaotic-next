2fd48509659b006f0e98c9d979aa40b0f9e68244
Nico Jensch <root@dr460nf1r3.org>
2025-01-18 14:51:26 +0100

    fix(backend): better container shutdown handling

--- entry_point.sh
+++ entry_point.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 
 export REDIS_PORT="${REDIS_PORT:-6379}"
+AUTOSSH_PID=
+NODE_PID=
 
-isRunning() {
+function isRunning() {
   if [ ! -f /tmp/tunnel.pid ]; then
     return 1
   fi
@@ -20,34 +22,58 @@ isRunning() {
   return 0
 }
 
-if [ -n "$REDIS_SSH_HOST" ]; then
-  REDIS_SSH_PORT="${REDIS_SSH_PORT:-22}"
-  REDIS_SSH_USER="${REDIS_SSH_USER:-root}"
+function init() {
+  if [ -n "$REDIS_SSH_HOST" ]; then
+    REDIS_SSH_PORT="${REDIS_SSH_PORT:-22}"
+    REDIS_SSH_USER="${REDIS_SSH_USER:-root}"
 
-  if ! isRunning; then
-    # Set up ssh tunneling
-    AUTOSSH_PIDFILE=/tmp/tunnel.pid AUTOSSH_GATETIME=0 AUTOSSH_PORT=0 autossh -f -N -L "6380:127.0.0.1:${REDIS_PORT}" \
-      -p "$REDIS_SSH_PORT" \
-      -o StrictHostKeyChecking=no \
-      -o ServerAliveInterval=60 \
-      -o ServerAliveCountMax=3 \
-      -o ExitOnForwardFailure=yes \
-      -o ConnectTimeout=10 \
-      -o TCPKeepAlive=yes \
-      -i /app/sshkey \
-      "$REDIS_SSH_USER@$REDIS_SSH_HOST"
-  else
-    echo "AutoSSH already running"
+    if ! isRunning; then
+      echo "Starting up authSSH..."
+      # Set up ssh tunneling
+      AUTOSSH_PIDFILE=/tmp/tunnel.pid AUTOSSH_GATETIME=0 AUTOSSH_PORT=0 autossh -f -N -L "6380:127.0.0.1:${REDIS_PORT}" \
+        -p "$REDIS_SSH_PORT" \
+        -o StrictHostKeyChecking=no \
+        -o ServerAliveInterval=60 \
+        -o ServerAliveCountMax=3 \
+        -o ExitOnForwardFailure=yes \
+        -o ConnectTimeout=10 \
+        -o TCPKeepAlive=yes \
+        -i /app/sshkey \
+        "$REDIS_SSH_USER@$REDIS_SSH_HOST"
+    else
+      echo "AutoSSH already running."
+    fi
+
+    export REDIS_PORT=6380
+
+    # Wait for tunnel to be established
+    echo "Waiting for tunnel to be established..."
+    while ! nc -z localhost $REDIS_PORT; do
+      sleep 1
+    done
+    echo "Tunnel established."
+
+    AUTOSSH_PID="$(cat /tmp/tunnel.pid)"
   fi
+}
 
-  export REDIS_PORT=6380
+function shutdown() {
+  echo "Caught sigterm, shutting down..."
+  kill -TERM "$NODE_PID" 2>/dev/null
+  wait "$NODE_PID"
+  kill -TERM "$AUTOSSH_PID" 2>/dev/null
+  wait "$AUTOSSH_PID"
+  exit 0
+}
 
-  # Wait for tunnel to be established
-  echo "Waiting for tunnel to be established..."
-  while ! nc -z localhost $REDIS_PORT; do
-    sleep 1
-  done
-  echo "Tunnel established."
-fi
+function main() {
+  init
 
-exec node /app/main.js "$@" | node_modules/.bin/pino-pretty -cSt SYS:HH:MM --ignore pid,hostname
+  echo "Starting app..."
+  exec node /app/main.js "$@" &
+  NODE_PID=$!
+  wait "$NODE_PID"
+}
+
+trap shutdown SIGINT SIGQUIT SIGTERM
+main "$@"
