cc5994b5c9039466a377064112e8b42cdda38d8d
Nico Jensch <root@dr460nf1r3.org>
2025-01-31 16:27:51 +0100

    feat: rewrite nestjs-omacache with NestJs and drop it

--- backend/src/gitlab/gitlab.service.ts
+++ backend/src/gitlab/gitlab.service.ts
@@ -1,52 +1,73 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { CACHE_GITLAB_TTL, PipelineWithExternalStatus } from '@./shared-lib';
 import { CommitStatusSchema, Gitlab, PipelineSchema } from '@gitbeaker/rest';
-import { PipelineWithExternalStatus } from '@./shared-lib';
+import { type Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Mutex } from 'async-mutex';
 
 @Injectable()
 export class GitlabService {
-  api = new Gitlab({
-    token: this.configService.getOrThrow<string>('CAUR_GITLAB_TOKEN'),
-  });
-
+  api: any;
   updateMutex = new Mutex();
 
-  private readonly chaoticId = this.configService.getOrThrow<string>('CAUR_GITLAB_ID_CAUR');
-  private readonly garudaId = this.configService.getOrThrow<string>('CAUR_GITLAB_ID_GARUDA');
+  private readonly CACHE_KEY_PIPELINES = 'gitlab/pipelines';
+  private readonly chaoticId: string;
 
-  constructor(private readonly configService: ConfigService) {}
+  // private readonly garudaId: string;
 
-  private isExternalStage(name: string): boolean {
-    return name.startsWith('chaotic-aur:') || name.startsWith('garuda:');
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly configService: ConfigService,
+  ) {
+    this.chaoticId = this.configService.getOrThrow<string>('CAUR_GITLAB_ID_CAUR');
+    // this.garudaId = this.configService.getOrThrow<string>('CAUR_GITLAB_ID_GARUDA');
+    this.api = new Gitlab({
+      token: this.configService.getOrThrow<string>('CAUR_GITLAB_TOKEN'),
+    });
   }
 
   /**
-   * Get the last GitLab pipelines for the chaotic-aur.
+   * Get the last GitLab pipelines for the chaotic-aur. Caches the result for CACHE_GITLAB_TTL (10s).
    * @returns The last pipelines with their external statuses (aka build logs)
    */
   async getLastPipelines(): Promise<PipelineWithExternalStatus[]> {
-    return await this.updateMutex.runExclusive(async () => {
-      try {
-        const fetchPromises: Promise<{ commit: CommitStatusSchema[]; pipeline: PipelineSchema }>[] = [];
+    let data: PipelineWithExternalStatus[] = await this.cacheManager.get<PipelineWithExternalStatus[]>(
+      this.CACHE_KEY_PIPELINES,
+    );
+    if (!data) {
+      data = await this.updateMutex.runExclusive(async () => {
+        try {
+          const fetchPromises: Promise<{ commit: CommitStatusSchema[]; pipeline: PipelineSchema }>[] = [];
 
-        let allPipelines: PipelineSchema[] = await this.api.Pipelines.all(this.chaoticId, {
-          maxPages: 1,
-          page: 1,
-          perPage: 50,
-        });
-        allPipelines = allPipelines.filter((pipeline) => pipeline.status !== 'skipped');
+          let allPipelines: PipelineSchema[] = await this.api.Pipelines.all(this.chaoticId, {
+            maxPages: 1,
+            page: 1,
+            perPage: 50,
+          });
+          allPipelines = allPipelines.filter((pipeline) => pipeline.status !== 'skipped');
 
-        for (const pipeline of allPipelines) {
-          this.getCommitStatus(pipeline, fetchPromises);
+          for (const pipeline of allPipelines) {
+            this.getCommitStatus(pipeline, fetchPromises);
+          }
+
+          const promiseResults: PipelineWithExternalStatus[] = await Promise.all(fetchPromises);
+          return promiseResults.sort((a, b) => b.pipeline.id - a.pipeline.id);
+        } catch (err) {
+          Logger.error(err, 'GitlabService');
         }
+      });
 
-        const promiseResults = await Promise.all(fetchPromises);
-        return promiseResults.sort((a, b) => b.pipeline.id - a.pipeline.id);
-      } catch (err) {
-        Logger.error(err, 'GitlabService');
-      }
-    });
+      await this.cacheManager.set(this.CACHE_KEY_PIPELINES, data, CACHE_GITLAB_TTL);
+    }
+    return data;
+  }
+
+  /**
+   * Bust the cache for the pipelines.
+   * @returns True if the cache was successfully busted, false otherwise
+   */
+  public async bustCache(): Promise<boolean> {
+    return await this.cacheManager.del(this.CACHE_KEY_PIPELINES);
   }
 
   /**
@@ -57,8 +78,10 @@ export class GitlabService {
   private getCommitStatus(pipeline: PipelineSchema, promiseArray: Promise<PipelineWithExternalStatus>[]) {
     promiseArray.push(
       new Promise((resolve) => {
-        this.api.Commits.allStatuses(this.chaoticId, pipeline.sha).then((statuses) => {
-          const onlyExternal: CommitStatusSchema[] = statuses.filter((status) => this.isExternalStage(status.name));
+        this.api.Commits.allStatuses(this.chaoticId, pipeline.sha).then((statuses: CommitStatusSchema[]) => {
+          const onlyExternal: CommitStatusSchema[] = statuses.filter((status: CommitStatusSchema) =>
+            this.isExternalStage(status.name),
+          );
           resolve({
             commit: onlyExternal.filter((status) => status.pipeline_id === pipeline.id),
             pipeline,
@@ -66,5 +89,14 @@ export class GitlabService {
         });
       }),
     );
+  }
+
+  /**
+   * Check if a stage is an external stage appended by our Chaotic Manager.
+   * @param name The name of the stage
+   * @private
+   */
+  private isExternalStage(name: string): boolean {
+    return name.startsWith('chaotic-aur:') || name.startsWith('garuda:');
   }
 }
