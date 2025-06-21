638f076ca6ce7b830965740e6e99779539a17211
Nico Jensch <root@dr460nf1r3.org>
2025-06-21 17:14:00 +0200

    chore: remove unused clutter

--- frontend/src/app/pipes/package-detail-key.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PackageDetailKeyPipe } from './package-detail-key.pipe';
-
-describe('PackageDetailKeyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PackageDetailKeyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- frontend/src/app/pipes/unix-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UnixDatePipe } from './unix-date.pipe';
-
-describe('UnixDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new UnixDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
