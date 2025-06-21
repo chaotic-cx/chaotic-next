1eb39ec9539a8f3f0dee17b17a49d1ada715fc2d
Nico Jensch <root@dr460nf1r3.org>
2025-06-21 14:38:27 +0200

    feat: update mirror map component with ngx-map integration and enhance loading logic

--- frontend/src/app/mirror-map/mirror-map.component.ts
+++ frontend/src/app/mirror-map/mirror-map.component.ts
@@ -1,23 +1,50 @@
-import { ChangeDetectionStrategy, Component, type ElementRef, inject, ViewChild } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { APP_CONFIG } from '../../environments/app-config.token';
-import { EnvironmentModel } from '../../environments/environment.model';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, signal } from '@angular/core';
+import { Dot, NgxMapComponent } from '@omnedia/ngx-map';
+import { AppService } from '../app.service';
+import { retry } from 'rxjs';
+import { flavors } from '@catppuccin/palette';
 
 @Component({
   selector: 'chaotic-mirror-map',
   templateUrl: './mirror-map.component.html',
   styleUrl: './mirror-map.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgxMapComponent],
 })
 export class MirrorMapComponent {
-  urlSafe: SafeResourceUrl;
+  protected dots: Dot[] = [];
+  protected readonly loading = signal<boolean>(true);
+  protected readonly lineColor = flavors.mocha.colors.mauve.hex;
+  protected readonly mapColor = flavors.mocha.colors.surface1.hex;
 
-  @ViewChild('iframe', { static: false }) iframe!: ElementRef;
-
-  private readonly appConfig: EnvironmentModel = inject(APP_CONFIG);
-  private readonly sanitizer = inject(DomSanitizer);
+  private readonly appService = inject(AppService);
+  private readonly cdr = inject(ChangeDetectorRef);
 
   constructor() {
-    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.appConfig.mapUrl);
+    this.appService
+      .getMirrorsStats()
+      .pipe(retry({ count: 3, delay: 5000 }))
+      .subscribe({
+        next: (data) => {
+          this.dots = data.mirrors.map((mirror) => {
+            return {
+              lat: mirror.latlon[0],
+              lng: mirror.latlon[1],
+              end: { lat: data.self.latlon[0], lng: data.self.latlon[1] },
+              start: { lat: mirror.latlon[0], lng: mirror.latlon[1] },
+              label: mirror.subdomain,
+              icon: 'https://chaotic.cx/favicon.ico',
+            };
+          });
+          this.loading.set(false);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+        complete: () => {
+          this.loading.set(false);
+          this.cdr.markForCheck();
+        },
+      });
   }
 }
