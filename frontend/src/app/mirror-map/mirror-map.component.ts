09db061186cf36ac3413ec01e410267ac8e4ac82
Nico Jensch <root@dr460nf1r3.org>
2025-09-30 15:36:05 +0200

    fix(mirror-map): incorrect handling of cdn-mirror, change icons again

--- frontend/src/app/mirror-map/mirror-map.component.ts
+++ frontend/src/app/mirror-map/mirror-map.component.ts
@@ -26,16 +26,18 @@ export class MirrorMapComponent {
       .pipe(retry({ count: 3, delay: 5000 }))
       .subscribe({
         next: (data) => {
-          this.dots = data.mirrors.map((mirror) => {
-            return {
-              lat: mirror.latlon.length > 0 ? mirror.latlon[0] : null,
-              lng: mirror.latlon.length > 0 ? mirror.latlon[1] : null,
-              end: { lat: data.self.latlon[0], lng: data.self.latlon[1] },
-              start: { lat: mirror.latlon[0], lng: mirror.latlon[1] },
-              label: mirror.subdomain,
-              icon: 'https://chaotic.cx/favicon.ico',
-            };
-          });
+          this.dots = data.mirrors
+            .filter((mirror) => mirror.latlon?.length > 0)
+            .map((mirror) => {
+              return {
+                lat: mirror.latlon[0],
+                lng: mirror.latlon[1],
+                end: { lat: data.self.latlon[0], lng: data.self.latlon[1] },
+                start: { lat: mirror.latlon[0], lng: mirror.latlon[1] },
+                label: mirror.subdomain,
+                icon: 'https://chaotic.cx/favicon.ico',
+              };
+            });
           this.loading.set(false);
         },
         error: (err) => {
