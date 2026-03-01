70c3f1e6645d66d4147b1747bbe1d4679320b236
Nico Jensch <root@dr460nf1r3.org>
2026-03-01 13:06:47 +0100

    fix(backend): order numerical

--- backend/src/router/router.service.ts
+++ backend/src/router/router.service.ts
@@ -10,28 +10,28 @@ export class RouterService {
 
   async getCountryStats(days: number): Promise<{ country: string; count: string }[]> {
     return this.dataSource.query(
-      `SELECT country, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY country ORDER BY count DESC`,
+      `SELECT country, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY country ORDER BY COUNT(*) DESC`,
       [nDaysInPast(days)],
     );
   }
 
   async getMirrorStats(days: number): Promise<{ mirror: string; count: string }[]> {
     return this.dataSource.query(
-      `SELECT hostname AS mirror, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY hostname ORDER BY count DESC`,
+      `SELECT hostname AS mirror, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY hostname ORDER BY COUNT(*) DESC`,
       [nDaysInPast(days)],
     );
   }
 
   async getPackageStats(days: number): Promise<{ pkgbase: string; count: string }[]> {
     return this.dataSource.query(
-      `SELECT package AS pkgbase, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY package ORDER BY count DESC`,
+      `SELECT package AS pkgbase, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY package ORDER BY COUNT(*) DESC`,
       [nDaysInPast(days)],
     );
   }
 
   async getPerDayStats(days: number): Promise<{ day: string; count: string }[]> {
     return this.dataSource.query(
-      `SELECT DATE(timestamp) AS day, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY DATE(timestamp) ORDER BY day ASC`,
+      `SELECT DATE(timestamp) AS day, COUNT(*)::text AS count FROM "router-hits" WHERE timestamp > $1 GROUP BY DATE(timestamp) ORDER BY COUNT(*) DESC`,
       [nDaysInPast(days)],
     );
   }
