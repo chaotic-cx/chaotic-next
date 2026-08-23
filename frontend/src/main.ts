8c939862178f454b3ea752cc3e6874b22a22fab4
Nico Jensch <root@dr460nf1r3.org>
2026-08-23 22:19:56 +0200

    chore: debug safari failure

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,4 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+declare global {
+  interface Window {
+    chaoticAppBooted?: boolean;
+  }
+}
+
+bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    window.chaoticAppBooted = true;
+  })
+  .catch((err: unknown) => console.error(err));
